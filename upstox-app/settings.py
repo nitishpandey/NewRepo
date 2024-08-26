da97369b5b4edd7a4cee8aa58284606ed88a957b
Shaurya <shauryapandey@gmail.com>
2024-08-26 15:45:51 +0530

    used longer notation to form static folder path. Gemini helped

--- upstox-app/settings.py
+++ upstox-app/settings.py
@@ -14,8 +14,8 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-STATIC_ROOT = BASE_DIR / 'static'
-
+#STATIC_ROOT = BASE_DIR / 'static'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
