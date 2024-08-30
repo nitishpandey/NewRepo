d39885b341b8181065764787e1046801577d1496
Shaurya <shauryapandey@gmail.com>
2024-08-30 21:38:32 +0530

    syntax error

--- polls/urls.py
+++ polls/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 
 from . import views
-from .viewclasses import defaultView, funds
+from .viewclasses import *
 
 urlpatterns = [
     path("", defaultView.as_view(), name="defaultViewClass"),
     
-    path("funds",funds.as_view(),name="fundsView")
+    path("funds",funds.as_view(),name="fundsView"),
     path("optionsdata",optionsdata.as_view(),name="optionsdataView")
 
 ]
