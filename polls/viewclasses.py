a6d3f9295b7a2e00a09fb778dc9d2738f61c623d
Shaurya <shauryapandey@gmail.com>
2024-08-31 07:14:03 +0530

    django.settings not required as of now because we are using apps module of django for global variables.

--- polls/viewclasses.py
+++ polls/viewclasses.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views import View
-from django.conf import settings
 from django.apps import apps
 import requests
 import json
