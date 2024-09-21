52ac4cd8bf880afc7ddd61e97d0f1fc38eac39d4
Shaurya <shauryapandey@gmail.com>
2024-09-21 09:49:11 +0530

    reference the fnoappbe rather than polls or upstox-app apps

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'upstox-app.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fnoappbe.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
